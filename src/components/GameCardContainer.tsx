f21b22b8f041ce521504340a8ce60be78d8f5922
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-27 22:43:19 -0600

    Display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react'
 
 const GameCardContainer = ({children}: PropsWithChildren) => {
   return (
-    <Box width='300px' borderRadius={10} overflow={"hidden"}>
+    <Box  borderRadius={10} overflow={"hidden"}>
         {children}
     </Box>
   )
