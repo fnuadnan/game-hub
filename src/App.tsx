dfeb87630606720c876674ab83c84f32bc877958
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-29 16:44:22 -0600

    Build sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
 import { Platform } from "./hooks/useGames";
 import { Genre } from "./hooks/useGenre";
 
 const App = () => {
-
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
@@ -22,22 +22,30 @@ const App = () => {
                "aside main"`,
         }}
         templateColumns={{
-          base: '1fr',
-          lg: '200px 1fr',
-          
+          base: "1fr",
+          lg: "200px 1fr",
         }}
       >
-        <GridItem area="nav" >
+        <GridItem area="nav">
           <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
-            <GenreList selectedGenre={selectedGenre} onSeclectGenre={(genre) => setSelectedGenre(genre)}/>
+            <GenreList
+              selectedGenre={selectedGenre}
+              onSeclectGenre={(genre) => setSelectedGenre(genre)}
+            />
           </GridItem>
         </Show>
         <GridItem area="main">
-          <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
-          <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
+          <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+            <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
+            <SortSelector />
+          </HStack>
+          <GameGrid
+            selectedPlatform={selectedPlatform}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Grid>
     </div>
